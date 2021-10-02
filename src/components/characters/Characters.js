6cc299780e7c5a2f8260e52b29607fd8268961c6
Al <alexseier12345@gmail..com>
2021-10-02 17:23:11 +0300

    add styles

--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -1,5 +1,31 @@
+import { Card, CardContent } from "@mui/material"
 import styles from "./characters.module.scss"
 
 export const Characters = () => {
-  return <div className={styles.characters}>lol</div>
+  return (
+    <div className={styles.characters}>
+      <div className={styles.container}>
+        <div className={styles.box}>
+          <Card
+            sx={{ backgroundColor: "#3c3e44", color: "#fff" }}
+            className={styles.card}
+          >
+            <img
+              className={styles.media}
+              src="https://rickandmortyapi.com/api/character/avatar/79.jpeg"
+              alt="Live from space album cover"
+            />
+            <CardContent>
+              <h4>Crab Spider</h4>
+              <p>Alive - Alien</p>
+              <h4>Crab Spider</h4>
+              <p>Alive - Alien</p>
+              <h4>Crab Spider</h4>
+              <p>Close Rick-counters of the Rick Kind</p>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  )
 }
